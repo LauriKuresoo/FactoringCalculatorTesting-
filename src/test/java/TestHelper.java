b3ad0ca35f892e2e9078048da92ded1d77debab3
Lauri Kuresoo <lauri.kuresoo@ut.ee>
2024-04-04 10:52:31 +0300

    Making helper methods to test the application

--- src/test/java/TestHelper.java
+++ src/test/java/TestHelper.java
@@ -1,27 +1,91 @@
 import org.junit.After;
 import org.junit.Before;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
+enum AdvanceRate {
+    a75("75"), a80("80"), a85("85"), a90("90");
+    public final String label;
+    AdvanceRate(String rate) {
+        this.label = rate;
+    }
+}
+
+enum PaymentTerm{
+    a30("30"), a60("60"), a90("90"), a120("120");
+    public final String label;
+    PaymentTerm(String term) {
+        this.label = term;
+    }
+}
+
 public class TestHelper {
 
     static WebDriver driver;
-
     String baseUrl = "https://www.swedbank.lt/business/finance/trade/factoring?language=ENG";
+
+    WebElement invoiceAmount = driver.findElement(By.id("D5"));
+    WebElement advanceRate = driver.findElement(By.id("D6"));
+    WebElement interestRate = driver.findElement(By.id("D7"));
+    WebElement paymentTerm = driver.findElement(By.id("D8"));
+    WebElement commisionFee = driver.findElement(By.id("D9"));
+
+
+
     @Before
     public void setUp(){
-
-        // if you use Chrome:
         System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\UserDrivers\\chromedriver-win64\\chromedriver.exe");
         driver = new ChromeDriver();
-
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.get(baseUrl);
-
     }
 
+    private void invoiceAmountFill(double invoiceAmount){
+        this.invoiceAmount.clear();
+        this.invoiceAmount.sendKeys(String.valueOf(invoiceAmount));
+    }
+
+    private void advanceRateFill(AdvanceRate rate){
+        this.advanceRate.clear();
+        this.advanceRate.sendKeys(rate.label);
+    }
+
+    private void interestRateFill(double interestRate){
+        this.interestRate.clear();
+        this.interestRate.sendKeys(Double.toString(interestRate));
+    }
+
+    private void paymentTermFill(PaymentTerm term){
+        this.paymentTerm.clear();
+        this.paymentTerm.sendKeys(term.label);
+    }
+
+    private void commissionFeeFill(double commissionFee){
+        this.commisionFee.clear();
+        this.commisionFee.sendKeys(Double.toString(commissionFee));
+    }
+
+
+    void fillInputFields(double invoiceAmount, AdvanceRate advanceRate, double interestRate, PaymentTerm paymentTerm, double commissionFee){
+        invoiceAmountFill(invoiceAmount);
+        advanceRateFill(advanceRate);
+        interestRateFill(interestRate);
+        paymentTermFill(paymentTerm);
+        commissionFeeFill(commissionFee);
+    }
+
+    double calculatateValidOutput(double invoiceAmount, AdvanceRate advanceRate, double interestRate, PaymentTerm paymentTerm, double commissionFee){
+        double answer = invoiceAmount *
+                (Double.valueOf(advanceRate.label)/100)
+                * (interestRate/100)
+                * (Double.valueOf(paymentTerm.label)/360)
+                + invoiceAmount * (commissionFee/100);
+        return answer;
+    }
 
 
 
--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,5 +1,17 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class Tests extends TestHelper{
 
 
+    @Test
+    public void allValidInputs(){
+        fillInputFields(1, AdvanceRate.a80, 1, PaymentTerm.a30, 1);
+        double correctOutput = calculatateValidOutput(1, AdvanceRate.a80, 1, PaymentTerm.a30, 1);
+        double applicationOutput = Double.valueOf(driver.findElement(By.id("result")).getText());
+        assertEquals(correctOutput, applicationOutput);
 
+    }
 }
